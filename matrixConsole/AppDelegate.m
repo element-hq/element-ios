cee1ddd2875bb0dc3d8704cef8426642c439730a
giomfo <giom@matrix.org>
2014-12-09 18:21:39 +0100

    Handle Matrix User presence as following: - set Online when app is active. - set Unavailable when app is suspended (inactive). - set Offline when user logs out the app (this option is currently commented because the user is kept offline when he logs in again from web client).
    Multidevice handling: the app compares the presence from server with the local value in order to keep the most pertinent.

--- matrixConsole/AppDelegate.m
+++ matrixConsole/AppDelegate.m
@@ -60,30 +60,28 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
     if (self.errorNotification) {
         [self.errorNotification dismiss:NO];
         self.errorNotification = nil;
     }
     
     // Suspend Matrix handler
-    [[MatrixHandler sharedHandler] pause];
+    [[MatrixHandler sharedHandler] pauseInBackgroundTask];
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    
-    // Resume Matrix handler
-    [[MatrixHandler sharedHandler] resume];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // Resume Matrix handler
+    [[MatrixHandler sharedHandler] resume];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
