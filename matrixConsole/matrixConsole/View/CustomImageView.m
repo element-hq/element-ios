7a11f1399ce4a04bed079e87f5ad5fce1d2bcc38
ylecollen <yannick@matrix.org>
2015-01-06 09:41:44 +0100

    Add the pitch to zoom gesture to a zoomed image

--- matrixConsole/matrixConsole/View/CustomImageView.m
+++ matrixConsole/matrixConsole/View/CustomImageView.m
@@ -33,10 +33,19 @@
     blockCustomImageView_onClick rightHandler;
     
     UIView* bottomBarView;
+
+    // sub items
+    UIScrollView* scrollView;
+    UIImageView* imageView;
+
+    // 
+    NSString* loadedImageURL;
+    UIImage* _image;
 }
 @end
 
 @implementation CustomImageView
+@synthesize canBeZoomed;
 
 #define CUSTOM_IMAGE_VIEW_BUTTON_WIDTH 100
 
@@ -100,6 +109,18 @@
 
 #pragma mark -
 
+// image setter/getter
+- (void)setImage:(UIImage *)anImage {
+    _image = anImage;
+    
+    imageView.image = anImage;
+    [self initScrollZoomFactors];
+}
+
+- (UIImage*)image {
+    return _image;
+}
+
 - (IBAction)onButtonToggle:(id)sender
 {
     if (sender == leftButton) {
@@ -133,11 +154,75 @@
     return button;
 }
 
+- (void)initScrollZoomFactors {
+    // check if the image can be zoomed
+    if (self.image && self.canBeZoomed && imageView.frame.size.width && imageView.frame.size.height) {
+        // ensure that the content size is properly initialized
+        scrollView.contentSize = scrollView.frame.size;
+        
+        // compute the appliable zoom factor
+        // assume that the user does not expect to zoom more than 100%
+        CGSize imageSize = self.image.size;
+        
+        CGFloat scaleX = imageSize.width  / imageView.frame.size.width;
+        CGFloat scaleY = imageSize.height / imageView.frame.size.height;
+        
+        if (scaleX < scaleY)
+        {
+            scaleX = scaleY;
+        }
+        
+        if (scaleX < 1.0)
+        {
+            scaleX = 1.0;
+        }
+        
+        scrollView.zoomScale        = 1.0;
+        scrollView.minimumZoomScale = 1.0;
+        scrollView.maximumZoomScale = scaleX;
+        
+        // update the image frame to ensure that it fits to the scrollview frame
+        imageView.frame = scrollView.bounds;
+    }
+}
+
 - (void)layoutSubviews {
     
     // call upper layer
     [super layoutSubviews];
 
+    // create the subviews if they don't exist
+    if (!scrollView) {
+        scrollView = [[UIScrollView alloc] init];
+        scrollView.delegate = self;
+        scrollView.backgroundColor = [UIColor clearColor];
+        [self addSubview:scrollView];
+        
+        imageView = [[UIImageView alloc] init];
+        imageView.backgroundColor = [UIColor clearColor];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        [scrollView addSubview:imageView];
+    }
+    
+    // the image has been updated
+    if (imageView.image != self.image) {
+        imageView.image = self.image;
+    }
+
+    // update the scrollview frame
+    CGRect oneSelfFrame = CGRectIntegral(self.frame);
+    CGRect scrollViewFrame = CGRectIntegral(scrollView.frame);
+    
+    oneSelfFrame.origin = scrollViewFrame.origin = CGPointZero;
+    
+    // use integral rect to avoid rounded value issue (float precision)
+    if (!CGRectEqualToRect(CGRectIntegral(self.frame), CGRectIntegral(scrollView.frame))) {
+        scrollView.frame = oneSelfFrame;
+        imageView.frame = oneSelfFrame;
+        
+        [self initScrollZoomFactors];
+    }
+    
     // check if the dedicated buttons are already added
     if (leftButtonTitle || rightButtonTitle) {
         
@@ -187,6 +272,17 @@
 }
 
 - (void)setImageURL:(NSString *)imageURL {
+    // the displayed image is already the expected one ?
+    if ([imageURL isEqualToString:loadedImageURL]) {
+        
+        // check if the image content has not been released
+        if (self.image.size.width && self.image.size.height) {
+            return;
+        }
+        
+        loadedImageURL = nil;
+    }
+    
     // Cancel media loader in progress (if any)
     if (imageLoader) {
         [MediaManager cancel:imageLoader];
@@ -201,6 +297,7 @@
         // Set picture placeholder
         self.image = [UIImage imageNamed:_placeholder];
     }
+    
     // Consider provided url to update image view
     if (imageURL) {
         // Load picture
@@ -208,9 +305,10 @@
             [self startActivityIndicator];
         }
         imageLoader = [MediaManager loadPicture:imageURL
-                                        success:^(UIImage *image) {
+                                        success:^(UIImage *anImage) {
                                             [self stopActivityIndicator];
-                                            self.image = image;
+                                            self.image = anImage;
+                                            loadedImageURL = imageURL;
                                         }
                                         failure:^(NSError *error) {
                                             [self stopActivityIndicator];
@@ -236,6 +334,13 @@
         [bottomBarView removeFromSuperview];
         bottomBarView = nil;
     }
+}
+
+#pragma mark - UIScrollViewDelegate
+// require to be able to zoom an image
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.canBeZoomed ? imageView : nil;
 }
 
 @end
