07d554b99278dd22af9746ec8e4fa044f99c39a2
ylecollen <yannick@matrix.org>
2015-01-12 14:19:23 +0100

    Fixe SYIOS-51 - spinner appears when backgrounding recents page

--- matrixConsole/matrixConsole/AppDelegate.m
+++ matrixConsole/matrixConsole/AppDelegate.m
@@ -92,14 +92,14 @@
         [self.errorNotification dismiss:NO];
         self.errorNotification = nil;
     }
-    
-    // Suspend Matrix handler
-    [[MatrixHandler sharedHandler] pauseInBackgroundTask];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    // Suspend Matrix handler
+    [[MatrixHandler sharedHandler] pauseInBackgroundTask];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
