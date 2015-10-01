cc4842d03cc332844a90d239d045ae7154dc16ed
giomfo <giom@matrix.org>
2015-10-01 10:36:22 +0200

    Fix: A constraint cannot be made between a leading/trailing attribute and a right/left attribute. Use leading/trailing for both or neither.'

--- Vector/ViewController/RoomViewController.m
+++ Vector/ViewController/RoomViewController.m
@@ -75,7 +75,7 @@
                                                                       attribute:NSLayoutAttributeLeading
                                                                       relatedBy:0
                                                                          toItem:self.view
-                                                                      attribute:NSLayoutAttributeLeft
+                                                                      attribute:NSLayoutAttributeLeading
                                                                      multiplier:1.0
                                                                        constant:0];
     
@@ -83,7 +83,7 @@
                                                                        attribute:NSLayoutAttributeTrailing
                                                                        relatedBy:0
                                                                           toItem:self.view
-                                                                       attribute:NSLayoutAttributeRight
+                                                                       attribute:NSLayoutAttributeTrailing
                                                                       multiplier:1.0
                                                                         constant:0];
     
