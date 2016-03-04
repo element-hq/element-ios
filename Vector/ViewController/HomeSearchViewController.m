d649f403ca2add7c850d512c3b412900444b5397
manuroe <manu@matrix.org>
2016-03-04 14:14:08 +0100

    Search result display: in the room VC, show only the room display name. No user action except the back button

--- Vector/ViewController/HomeSearchViewController.m
+++ Vector/ViewController/HomeSearchViewController.m
@@ -16,6 +16,8 @@
 
 #import "HomeSearchViewController.h"
 
+#import "HomeViewController.h"
+
 #import "HomeSearchCellData.h"
 #import "HomeSearchTableViewCell.h"
 
@@ -72,6 +74,9 @@
     id<MXKSearchCellDataStoring> cellData = [self.dataSource cellDataAtIndex:indexPath.row];
     _selectedEvent = cellData.searchResult.result;
 
+    // Hide the keyboard handled by the search text input which belongs to HomeViewController
+    [((HomeViewController*)self.parentViewController).searchBar resignFirstResponder];
+    
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 
     // Make the HomeViewController (that contains this VC) open the RoomViewController
