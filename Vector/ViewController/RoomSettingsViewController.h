8c71ddcbbd36ce8d1518fe7b790f7882869ad5e2
giomfo <giom@matrix.org>
2016-08-22 11:54:26 +0200

    Bug Fix - Expanded header: Add shortcuts on avatar and room name
    #347

--- Vector/ViewController/RoomSettingsViewController.h
+++ Vector/ViewController/RoomSettingsViewController.h
@@ -19,7 +19,38 @@
 #import "MediaPickerViewController.h"
 #import "TableViewCellWithCheckBoxes.h"
 
+/**
+ List the settings fields. Used to preselect/edit a field
+ */
+typedef enum : NSUInteger {
+    /**
+     Default.
+     */
+    RoomSettingsViewControllerFieldNone,
+    
+    /**
+     The room name.
+     */
+    RoomSettingsViewControllerFieldName,
+    
+    /**
+     The room topic.
+     */
+    RoomSettingsViewControllerFieldTopic,
+    
+    /**
+     The room avatar.
+     */
+    RoomSettingsViewControllerFieldAvatar
+    
+} RoomSettingsViewControllerField;
+
 @interface RoomSettingsViewController : MXKRoomSettingsViewController <UITextViewDelegate, UITextFieldDelegate, MediaPickerViewControllerDelegate, MXKRoomMemberDetailsViewControllerDelegate, TableViewCellWithCheckBoxesDelegate>
+
+/**
+ Select a settings field in order to edit it ('RoomSettingsViewControllerFieldNone' by default).
+ */
+@property (nonatomic) RoomSettingsViewControllerField selectedRoomSettingsField;
 
 @end
 
