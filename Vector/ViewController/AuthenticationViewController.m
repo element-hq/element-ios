a0e0386d8c11dfc116b5943e68bc0b22b4f67129
giomfo <giom@matrix.org>
2016-09-16 14:27:20 +0200

    Bug Fix - Login screen: Stuck on flashing loading wheel
    in case of invalid credentials
    
    #637

--- Vector/ViewController/AuthenticationViewController.m
+++ Vector/ViewController/AuthenticationViewController.m
@@ -287,7 +287,7 @@
     // Homeserver migration: When the default homeserver url is different from matrix.org,
     // the login (or forgot pwd) process with an existing matrix.org accounts will then fail.
     // Patch: Falling back to matrix.org HS so we don't break everyone's logins
-    if ([self.homeServerTextField.text isEqualToString:self.defaultHomeServerUrl] && ![self.defaultHomeServerUrl isEqualToString:@"matrix.org"])
+    if ([self.homeServerTextField.text isEqualToString:self.defaultHomeServerUrl] && ![self.defaultHomeServerUrl isEqualToString:@"https://matrix.org"])
     {
         MXError *mxError = [[MXError alloc] initWithNSError:error];
         
