030e87872cca984b2b1d2ab05074dc72305b6725
giomfo <giom@matrix.org>
2016-09-30 11:17:12 +0200

    Enhancement - Messages search display
    - display the full text message
    - display room name and sender name
    - display attachment if any
    - refresh the search result on new joined room or left room.

---
+++ Vector/Views/MessagesSearchResult/MessagesSearchResultAttachmentBubbleCell.m
@@ -0,0 +1,54 @@
+/*
+ Copyright 2016 OpenMarket Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MessagesSearchResultAttachmentBubbleCell.h"
+
+#import "VectorDesignValues.h"
+
+#import "MXRoom+Vector.h"
+
+@implementation MessagesSearchResultAttachmentBubbleCell
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    self.userNameLabel.textColor = kVectorTextColorBlack;
+    
+    self.roomNameLabel.textColor = kVectorTextColorGray;
+    
+    self.messageTextView.tintColor = kVectorColorGreen;
+}
+
+- (void)render:(MXKCellData *)cellData
+{
+    [super render:cellData];
+    
+    if (bubbleData)
+    {
+        MXRoom* room = [bubbleData.mxSession roomWithRoomId:bubbleData.roomId];
+        if (room)
+        {
+            self.roomNameLabel.text = room.vectorDisplayname;
+        }
+        else
+        {
+            self.roomNameLabel.text = bubbleData.roomId;
+        }
+    }
+}
+
+@end
