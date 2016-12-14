7d1108dbfc55bf553302b2066dacad32fd1a5f2a
manuroe <manu@matrix.org>
2016-12-14 16:50:10 +0100

    Crypto threading: Follow changes in the sdk

--- Vector/ViewController/RoomMemberDetailsViewController.m
+++ Vector/ViewController/RoomMemberDetailsViewController.m
@@ -346,7 +346,12 @@
         }
         
         // Retrieve member's devices
-        devicesArray = [self.mxRoom.mxSession.crypto storedDevicesForUser:self.mxRoomMember.userId];
+        [self.mxRoom.mxSession.crypto devicesForUser:self.mxRoomMember.userId complete:^(NSArray<MXDeviceInfo *> *devices) {
+            devicesArray = devices;
+
+            // Reload the full table to take into account a potential change on a device status.
+            [super updateMemberInfo];
+        }];
     }
     
     // Complete data update and reload table view
@@ -934,7 +939,11 @@
     }
     else
     {
-        [self.mxRoom.mxSession.crypto setDeviceVerification:verificationStatus forDevice:deviceTableViewCell.deviceInfo.deviceId ofUser:self.mxRoomMember.userId];
+        [self.mxRoom.mxSession.crypto setDeviceVerification:verificationStatus
+                                                  forDevice:deviceTableViewCell.deviceInfo.deviceId
+                                                     ofUser:self.mxRoomMember.userId
+                                                    success:nil failure:nil
+         ];
         [self updateMemberInfo];
     }
 }
