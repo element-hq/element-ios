18a2576b9c17027b0c72b81e82df226464b8bf93
Denis Morozov <dmorozkn@gmail.com>
2017-07-30 16:19:27 +0300

    Update IntentHandler

--- SiriIntents/IntentHandler.m
+++ SiriIntents/IntentHandler.m
@@ -16,20 +16,10 @@
 
 #import "IntentHandler.h"
 
-#import "MXKAccountManager.h"
 #import "MXKAccount.h"
-#import "MXSession.h"
+#import "MXKAccountManager.h"
 #import "MXFileStore.h"
-//#import <MatrixKit/MXKAccountManager.h>
-
-// As an example, this class is set up to handle Message intents.
-// You will want to replace this or add other intents as appropriate.
-// The intents you wish to handle must be declared in the extension's Info.plist.
-
-// You can test your example integration by saying things to Siri like:
-// "Send a message using <myApp>"
-// "<myApp> John saying hello"
-// "Search for messages in <myApp>"
+#import "MXSession.h"
 
 @interface IntentHandler () <INStartAudioCallIntentHandling>
 
@@ -37,74 +27,137 @@
 
 @implementation IntentHandler
 
-- (id)handlerForIntent:(INIntent *)intent {
-    id handler = nil;
-    
-    if ([intent isKindOfClass:INStartAudioCallIntent.class])
-    {
-        handler = self;
-    }
-    
-    return handler;
+- (id)handlerForIntent:(INIntent *)intent
+{
+    return self;
 }
 
 #pragma mark - INStartAudioCallIntentHandling
 
 - (void)resolveContactsForStartAudioCall:(INStartAudioCallIntent *)intent withCompletion:(void (^)(NSArray<INPersonResolutionResult *> * _Nonnull))completion
 {
-    MXKAccount *account = [MXKAccountManager sharedManager].activeAccounts.firstObject;
-    MXSession *session = [[MXSession alloc] initWithMatrixRestClient:account.mxRestClient];
-    [session setStore:[[MXFileStore alloc] init] success:^{
-        NSLog(@"Super");
-    } failure:^(NSError *error) {
-        NSLog(@"Fail");
-    }];
-    
-    //    NSArray<INPerson *> *recipients = intent.contacts;
-    //    // If no recipients were provided we'll need to prompt for a value.
-    //    if (recipients.count == 0) {
-    //        completion(@[[INPersonResolutionResult needsValue]]);
-    //        return;
-    //    }
-    //    NSMutableArray<INPersonResolutionResult *> *resolutionResults = [NSMutableArray array];
-    //
-    //    for (INPerson *recipient in recipients) {
-    //        NSArray<INPerson *> *matchingContacts = @[recipient]; // Implement your contact matching logic here to create an array of matching contacts
-    //        if (matchingContacts.count > 1) {
-    //            // We need Siri's help to ask user to pick one from the matches.
-    //            [resolutionResults addObject:[INPersonResolutionResult disambiguationWithPeopleToDisambiguate:matchingContacts]];
-    //
-    //        } else if (matchingContacts.count == 1) {
-    //            // We have exactly one matching contact
-    //            [resolutionResults addObject:[INPersonResolutionResult successWithResolvedPerson:recipient]];
-    //        } else {
-    //            // We have no contacts matching the description provided
-    //            [resolutionResults addObject:[INPersonResolutionResult unsupported]];
-    //        }
-    //    }
-    //
-    //    completion(resolutionResults);
+    NSArray<INPerson *> *contacts = intent.contacts;
+    if (contacts.count == 0)
+    {
+        completion(@[[INPersonResolutionResult needsValue]]);
+        return;
+    }
+    else
+    {
+        // We don't iterate over array of contacts from passed intent
+        // since it's hard to imagine scenario with several callee
+        // so we just extract the first one
+        INPerson *callee = contacts.firstObject;
+        
+        // Check if the user has selected right callee among several candidates from previous resolution process run
+        if (callee.customIdentifier && callee.customIdentifier.length)
+        {
+            completion(@[[INPersonResolutionResult successWithResolvedPerson:callee]]);
+            return;
+        }
+        
+        MXKAccount *account = [MXKAccountManager sharedManager].activeAccounts.firstObject;
+        if (account)
+        {
+            MXSession *session = [[MXSession alloc] initWithMatrixRestClient:account.mxRestClient];
+            [session setStore:[[MXFileStore alloc] init] success:^{
+                
+                // Find all users with whom direct chats are existed
+                NSMutableSet<NSString *> *directUserIDs = [NSMutableSet set];
+                for (MXRoom *room in session.rooms)
+                {
+                    if (room.directUserId)
+                        [directUserIDs addObject:room.directUserId];
+                }
+                
+                NSMutableArray<INPerson *> *matchingPersons = [NSMutableArray array];
+                for (NSString *userID in directUserIDs.allObjects)
+                {
+                    MXUser *user = [session userWithUserId:userID];
+                    if (!user.displayname)
+                        continue;
+                    
+                    if (!NSEqualRanges([user.displayname rangeOfString:callee.displayName options:NSCaseInsensitiveSearch], (NSRange){NSNotFound,0}))
+                    {
+                        INPersonHandle *personHandle = [[INPersonHandle alloc] initWithValue:user.userId type:INPersonHandleTypeUnknown];
+                        INPerson *person = [[INPerson alloc] initWithPersonHandle:personHandle
+                                                                   nameComponents:nil
+                                                                      displayName:user.displayname
+                                                                            image:nil
+                                                                contactIdentifier:nil
+                                                                 customIdentifier:user.userId];
+                        
+                        [matchingPersons addObject:person];
+                    }
+                }
+                
+                if (matchingPersons.count == 0)
+                {
+                    completion(@[[INPersonResolutionResult unsupported]]);
+                }
+                else if (matchingPersons.count == 1)
+                {
+                    completion(@[[INPersonResolutionResult successWithResolvedPerson:matchingPersons.firstObject]]);
+                }
+                else
+                {
+                    completion(@[[INPersonResolutionResult disambiguationWithPeopleToDisambiguate:matchingPersons]]);
+                }
+                
+            } failure:^(NSError *error) {
+                // TODO: Maybe handle this in another way
+                completion(@[[INPersonResolutionResult unsupported]]);
+            }];
+        }
+        else
+        {
+            // If user hasn't logged in yet just pass a blank INPerson instance and handle this situation in confirmStartAudioCall:completion:
+            completion(@[[INPersonResolutionResult successWithResolvedPerson:[INPerson new]]]);
+        }
+    }
 }
 
 - (void)confirmStartAudioCall:(INStartAudioCallIntent *)intent completion:(void (^)(INStartAudioCallIntentResponse * _Nonnull))completion
 {
     INStartAudioCallIntentResponse *response = nil;
     
+    MXKAccount *account = [MXKAccountManager sharedManager].activeAccounts.firstObject;
+    if (account)
+    {
 #if defined MX_CALL_STACK_OPENWEBRTC || defined MX_CALL_STACK_ENDPOINT || defined MX_CALL_STACK_JINGLE
-    NSUserActivity *userActivity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass(INStartAudioCallIntent.class)];
-    response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeReady userActivity:userActivity];
+        NSUserActivity *userActivity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass(INStartAudioCallIntent.class)];
+        response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeReady userActivity:userActivity];
 #else
-    response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeFailureCallingServiceNotAvailable userActivity:nil];
+        response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeFailureCallingServiceNotAvailable userActivity:nil];
 #endif
+    }
+    else
+    {
+        // User hasn't logged in
+        response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeFailureRequiringAppLaunch userActivity:nil];
+    }
     
     completion(response);
 }
 
 - (void)handleStartAudioCall:(INStartAudioCallIntent *)intent completion:(void (^)(INStartAudioCallIntentResponse * _Nonnull))completion
 {
-    NSUserActivity *userActivity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass(INStartAudioCallIntent.class)];
-    //    userActivity.userInfo = @{ @"handle": [NSString stringWithFormat:@"TGCA%d", next.firstObject.userId] };
-    INStartAudioCallIntentResponse *response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeContinueInApp userActivity:userActivity];
+    INStartAudioCallIntentResponse *response = nil;
+    
+    INPerson *person = intent.contacts.firstObject;
+    if (person && person.customIdentifier)
+    {
+        NSUserActivity *userActivity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass(INStartAudioCallIntent.class)];
+        userActivity.userInfo = @{ @"userID" : person.customIdentifier };
+        
+        response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeContinueInApp
+                                                           userActivity:userActivity];
+    }
+    else
+    {
+        response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeFailure userActivity:nil];
+    }
+
     completion(response);
 }
 
