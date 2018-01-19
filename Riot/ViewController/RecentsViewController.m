3afc1915dde93458239fcd6acda6c1b9d70d6282
tellowkrinkle <tellowkrinkle@users.noreply.github.com>
2018-01-18 20:10:40 -0600

    Cancel Button uses style Cancel
    This allows you to tap outside of the popup to cancel

--- Riot/ViewController/RecentsViewController.m
+++ Riot/ViewController/RecentsViewController.m
@@ -1599,7 +1599,7 @@
                                                    }]];
     
     [currentAlert addAction:[UIAlertAction actionWithTitle:[NSBundle mxk_localizedStringForKey:@"cancel"]
-                                                     style:UIAlertActionStyleDefault
+                                                     style:UIAlertActionStyleCancel
                                                    handler:^(UIAlertAction * action) {
                                                        
                                                        if (weakSelf)
