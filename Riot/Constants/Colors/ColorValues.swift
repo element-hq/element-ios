a29251620d29aded0fd14b1279db5e0dfdaf3ec3
manuroe <manu@matrix.org>
2018-11-29 14:09:12 +0100

    Reskin: Store color values provided in zeplin

---
+++ Riot/Constants/Colors/ColorValues.swift
@@ -0,0 +1,57 @@
+/*
+ Copyright 2018 New Vector Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import UIKit
+
+/// Provide color constant values defined by the designer
+/// https://app.zeplin.io/project/5b857c64b1747a2c472290da/screen/5bf2cc89a4a6973f47883c6e
+@objc protocol ColorValues {
+
+    var background: UIColor { get }
+    var base: UIColor { get }
+
+    var searchBackground: UIColor { get }
+    var searchText: UIColor { get }
+
+    var headerBackground: UIColor { get }
+    var headerBorder: UIColor { get }
+    var headerTextPrimary: UIColor { get }
+    var headerTextSecondary: UIColor { get }
+
+    var textPrimary: UIColor { get }
+    var textSecondary: UIColor { get }
+
+    var accent: UIColor { get }
+    var unreadRoomIndent: UIColor { get }
+    
+    var notificationUnread: UIColor { get }
+    var notificationMention: UIColor { get }
+
+    var avatarColor1: UIColor { get }
+    var avatarColor2: UIColor { get }
+    var avatarColor3: UIColor { get }
+}
+
+extension UIColor {
+    convenience init(rgb: UInt) {
+        self.init(
+            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgb & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
---
+++ Riot/Constants/Colors/DefaultColorValues.swift
@@ -0,0 +1,50 @@
+/*
+ Copyright 2018 New Vector Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import Foundation
+import UIKit
+
+/// Color constants for the default theme
+@objcMembers
+final class DefaultColorValues: NSObject, ColorValues {
+
+    static let shared = DefaultColorValues()
+
+    let background: UIColor = UIColor(rgb: 0xFFFFFF)
+
+    let base: UIColor = UIColor(rgb: 0x2E3648)
+
+    let searchBackground: UIColor = UIColor(rgb: 0xFFFFFF)
+    let searchText: UIColor = UIColor(rgb: 0xACB3C2)
+
+    let headerBackground: UIColor = UIColor(rgb: 0xF1F5F8)
+    let headerBorder: UIColor  = UIColor(rgb: 0xEAEEF2)
+    let headerTextPrimary: UIColor = UIColor(rgb: 0x96A1B7)
+    let headerTextSecondary: UIColor = UIColor(rgb: 0xC8C8CD)
+
+    let textPrimary: UIColor = UIColor(rgb: 0x383838)
+    let textSecondary: UIColor = UIColor(rgb: 0x9E9E9E)
+
+    let accent: UIColor = UIColor(rgb: 0x7AC9A1)
+    let unreadRoomIndent: UIColor = UIColor(rgb: 0x2E3648)
+    
+    let notificationUnread: UIColor = UIColor(rgb: 0x7AC9A1)
+    let notificationMention: UIColor = UIColor(rgb: 0xF56679)
+
+    let avatarColor1: UIColor = UIColor(rgb: 0x7AC9A1)
+    let avatarColor2: UIColor = UIColor(rgb: 0x1E7DDC)
+    let avatarColor3: UIColor = UIColor(rgb: 0x76DDD7)
+}
