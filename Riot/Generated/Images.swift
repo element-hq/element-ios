b737addfd10bf006623a37dba5644d1a48cf7a44
SBiOSoftWhare <steveb@matrix.org>
2019-01-21 23:32:51 +0100

    Add reveal password button image

--- Riot/Generated/Images.swift
+++ Riot/Generated/Images.swift
@@ -59,6 +59,7 @@ internal enum Asset {
     internal static let priorityHigh = ImageAsset(name: "priorityHigh")
     internal static let priorityLow = ImageAsset(name: "priorityLow")
     internal static let createRoom = ImageAsset(name: "create_room")
+    internal static let revealPasswordButton = ImageAsset(name: "reveal_password_button")
     internal static let launchScreenRiot = ImageAsset(name: "LaunchScreenRiot")
     internal static let cameraCapture = ImageAsset(name: "camera_capture")
     internal static let cameraPlay = ImageAsset(name: "camera_play")
