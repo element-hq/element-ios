599bab3967801529cf7961d9d0a02262d192581c
SBiOSoftWhare <steveb@matrix.org>
2019-05-29 12:59:46 +0200

    MXKRoomBubbleTableViewCell: Display timestamp on left when select memberships.

--- Riot/Categories/MXKRoomBubbleTableViewCell+Riot.m
+++ Riot/Categories/MXKRoomBubbleTableViewCell+Riot.m
@@ -58,7 +58,9 @@ NSString *const kMXKRoomBubbleCellTapOnReceiptsContainer = @"kMXKRoomBubbleCellT
         }
         
         // Display timestamp on the left for selected component when it cannot overlap other UI elements like user's avatar
-        BOOL displayLabelOnLeft = roomBubbleCellData.displayTimestampForSelectedComponentOnLeftWhenPossible && !isFirstDisplayedComponent && !isLastMessageMostRecentComponent;
+        BOOL displayLabelOnLeft = roomBubbleCellData.displayTimestampForSelectedComponentOnLeftWhenPossible
+        && !isLastMessageMostRecentComponent
+        && ((isFirstDisplayedComponent && roomBubbleCellData.shouldHideSenderInformation) || !isFirstDisplayedComponent);
         
         [self addTimestampLabelForComponentIndex:componentIndex
                        isFirstDisplayedComponent:isFirstDisplayedComponent
