884f47b33e51c3980abd05a58888e8597f5c1448
SBiOSoftWhare <steveb@matrix.org>
2019-07-04 19:03:39 +0200

    Message edits history: Create header view.

---
+++ Riot/Modules/Room/EditHistory/EditHistoryHeaderView.swift
@@ -0,0 +1,36 @@
+/*
+ Copyright 2019 New Vector Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import UIKit
+import Reusable
+
+final class EditHistoryHeaderView: UITableViewHeaderFooterView, NibLoadable, Reusable, Themable {
+    
+    // MARK: - Properties
+    
+    @IBOutlet private weak var dateLabel: UILabel!
+    
+    // MARK: - Public
+    
+    func update(theme: Theme) {
+        self.contentView.backgroundColor = theme.backgroundColor
+        self.dateLabel.textColor = theme.headerTextPrimaryColor
+    }
+    
+    func fill(with dateString: String) {
+        self.dateLabel.text = dateString
+    }
+}
