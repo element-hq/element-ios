8dca707d8233f3f9fe7eb81c55974fefefc97403
SBiOSoftWhare <steveb@matrix.org>
2019-07-25 16:27:28 +0200

    Create SerializationService that enables to deserialize or serialize an object conforming to Codable.

---
+++ Riot/Managers/Serialization/SerializationService.swift
@@ -0,0 +1,35 @@
+/*
+ Copyright 2019 New Vector Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import Foundation
+
+final class SerializationService: SerializationServiceType {
+    
+    // MARK: - Properties
+    
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+    
+    // MARK: - Public
+    
+    func deserialize<T: Decodable>(_ data: Data) throws -> T {
+        return try decoder.decode(T.self, from: data)
+    }
+    
+    func serialize<T: Encodable>(_ object: T) throws -> Data {
+        return try encoder.encode(object)
+    }
+}
