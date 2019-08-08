ac1858a73001a536374a7e818504323b061a0111
manuroe <manu@matrix.org>
2019-08-08 15:05:50 +0200

    iOS13 code cannot be built from Xcode10

--- Riot/Categories/UISearchBar.swift
+++ Riot/Categories/UISearchBar.swift
@@ -20,10 +20,15 @@ extension UISearchBar {
     
     /// Returns internal UITextField
     @objc var vc_searchTextField: UITextField? {
-        if #available(iOS 13.0, *) {
-            return self.searchTextField
-        } else {
+        // TODO: To remove once on XCode11/iOS13
+        #if swift(>=5.1)
+            if #available(iOS 13.0, *) {
+                return self.searchTextField
+            } else {
+                return self.value(forKey: "searchField") as? UITextField
+            }
+        #else
             return self.value(forKey: "searchField") as? UITextField
-        }
+        #endif
     }
 }
