82c1578aaaa5b54b1ee0d152351a6bca018734bc
manuroe <manu@matrix.org>
2020-01-30 17:07:53 +0100

    Room Shield: Use sync version of [MXRoom membersTrustLevelSummary:]
    https://github.com/vector-im/riot-ios/issues/2956
    to break keys downloads in loop

--- Riot/Modules/Room/DataSources/RoomDataSource.m
+++ Riot/Modules/Room/DataSources/RoomDataSource.m
@@ -205,7 +205,7 @@
     
     if (userId)
     {
-        [self encryptionTrustLevelDidChangeRelatedToUserId:userId];
+        [self encryptionTrustLevelDidChangeRelatedToUserId:userId forceDownload:NO];
     }
 }
 
@@ -217,16 +217,16 @@
     
     if (userId)
     {
-        [self encryptionTrustLevelDidChangeRelatedToUserId:userId];
+        [self encryptionTrustLevelDidChangeRelatedToUserId:userId forceDownload:NO];
     }
 }
 
 - (void)fetchEncryptionTrustedLevel
 {
-    [self encryptionTrustLevelDidChangeRelatedToUserId:self.mxSession.myUser.userId];
+    [self encryptionTrustLevelDidChangeRelatedToUserId:self.mxSession.myUser.userId forceDownload:YES];
 }
 
-- (void)encryptionTrustLevelDidChangeRelatedToUserId:(NSString*)userId
+- (void)encryptionTrustLevelDidChangeRelatedToUserId:(NSString*)userId forceDownload:(BOOL)forceDownload
 {
     if (!self.room.summary.isEncrypted)
     {
@@ -239,7 +239,7 @@
         // If user belongs to the room refresh the trust level
         if (roomMember)
         {            
-            [self.room membersTrustLevelSummaryWithSuccess:^(MXUsersTrustLevelSummary *usersTrustLevelSummary) {
+            [self.room membersTrustLevelSummaryWithForceDownload:forceDownload success:^(MXUsersTrustLevelSummary *usersTrustLevelSummary) {
                 
                 RoomEncryptionTrustLevel roomEncryptionTrustLevel;
                 
