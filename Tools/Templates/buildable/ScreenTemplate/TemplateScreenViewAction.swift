a6db82e168a52ca886b4291b644f9e7c8d9adb88
SBiOSoftWhare <steveb@matrix.org>
2020-04-10 11:36:25 +0200

    Update screen templates

--- Tools/Templates/buildable/ScreenTemplate/TemplateScreenViewAction.swift
+++ Tools/Templates/buildable/ScreenTemplate/TemplateScreenViewAction.swift
@@ -18,7 +18,7 @@ import Foundation
 
 /// TemplateScreenViewController view actions exposed to view model
 enum TemplateScreenViewAction {
-    case sayHello
+    case loadData
     case complete
     case cancel
 }
