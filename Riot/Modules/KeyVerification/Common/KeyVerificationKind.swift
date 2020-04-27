16772c8eecfb61fa9f6604ea7c594845a83d415e
SBiOSoftWhare <steveb@matrix.org>
2020-04-27 19:46:44 +0200

    KeyVerificationKind: Add new cases.

--- Riot/Modules/KeyVerification/Common/KeyVerificationKind.swift
+++ Riot/Modules/KeyVerification/Common/KeyVerificationKind.swift
@@ -16,6 +16,26 @@
 import Foundation
 
 enum KeyVerificationKind {
-    case device
-    case user
+    case otherSession // An other session
+    case thisSession  // My current session is new
+    case newSession   // My other session is new
+    case user         // Another user
+    
+    var verificationTitle: String {
+        
+        let title: String
+        
+        switch self {
+        case .otherSession:
+            title = VectorL10n.keyVerificationOtherSessionTitle
+        case .thisSession:
+            title = VectorL10n.keyVerificationThisSessionTitle
+        case .newSession:
+            title = VectorL10n.keyVerificationNewSessionTitle
+        case .user:
+            title = VectorL10n.keyVerificationUserTitle
+        }
+        
+        return title
+    }
 }
