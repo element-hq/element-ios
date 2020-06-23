df8d91fcec2d256a3576b4f1fb2dbe902423bd21
ismailgulek <ismailg@matrix.org>
2020-06-23 11:36:25 +0300

    Refactoring and some docs to DataProtectionHelper

--- Riot/Utils/DataProtectionHelper.swift
+++ Riot/Utils/DataProtectionHelper.swift
@@ -18,30 +18,30 @@ import Foundation
 
 final class DataProtectionHelper {
     
+    /// Detects after reboot, before unlocked state. Does this by trying to write a file to the filesystem (to the Caches directory) and read it back.
+    /// - Parameter appGroupIdentifier: App-group identifier to be used when deciding where it'll try to write the file.
+    /// - Returns: true if the state detected
     static func isDeviceInRebootedAndLockedState(appGroupIdentifier: String? = nil) -> Bool {
         do {
-            let str = String.unique
-            let dummyData = str.data(using: .utf8)!
+            let dummyString = String.unique
+            let dummyData = dummyString.data(using: .utf8)!
 
             var url: URL!
-            if let identifier = appGroupIdentifier {
-                let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: identifier)
-                if containerURL == nil {
-                    url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
-                } else {
-                    url = containerURL?.appendingPathComponent("Library/Caches")
-                }
+            if let identifier = appGroupIdentifier,
+                let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: identifier) {
+                url = containerURL
             } else {
                 url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
             }
 
+            //  add a unique filename
             url = url.appendingPathComponent(String.unique)
 
             try dummyData.write(to: url, options: .completeFileProtectionUntilFirstUserAuthentication)
             let readData = try Data(contentsOf: url)
             let readString = String(data: readData, encoding: .utf8)
             try FileManager.default.removeItem(at: url)
-            if readString != str {
+            if readString != dummyString {
                 return true
             }
         } catch {
