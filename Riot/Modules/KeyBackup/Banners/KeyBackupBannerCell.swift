f180ef30f12e9354a9340957e7a368b18e35b974
SBiOSoftWhare <steveb@matrix.org>
2020-06-25 09:50:44 +0200

    Key backup banner: Update UI and strings.

--- Riot/Modules/KeyBackup/Banners/KeyBackupBannerCell.swift
+++ Riot/Modules/KeyBackup/Banners/KeyBackupBannerCell.swift
@@ -55,7 +55,7 @@ final class KeyBackupBannerCell: MXKTableViewCell {
         self.closeButton.tintColor = theme.textPrimaryColor
         
         self.titleLabel.textColor = theme.textPrimaryColor
-        self.subtitleLabel.textColor = theme.tintColor
+        self.subtitleLabel.textColor = theme.textPrimaryColor
     }
     
     // MARK: - Life cycle
@@ -63,8 +63,8 @@ final class KeyBackupBannerCell: MXKTableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        let shieldImage = Asset.Images.keyBackupLogo.image.withRenderingMode(.alwaysTemplate)
-        self.shieldImageView.image = shieldImage
+        let shieldImage = Asset.Images.secretsSetupKey.image.withRenderingMode(.alwaysTemplate)
+        self.shieldImageView.image = shieldImage 
         
         let closeImage = Asset.Images.closeBanner.image.withRenderingMode(.alwaysTemplate)
         self.closeButton.setImage(closeImage, for: .normal)
@@ -84,7 +84,7 @@ final class KeyBackupBannerCell: MXKTableViewCell {
         case .recover:
             title = VectorL10n.keyBackupRecoverBannerTitle
             subtitle = VectorL10n.keyBackupRecoverConnentBannerSubtitle
-        case .none:
+        default:
             title = nil
             subtitle = nil
         }
