74460ef65843b0f63e8b301da185a16037ecf0db
ismailgulek <ismailg@matrix.org>
2020-08-11 21:17:48 +0300

    Fix NSE bugs

--- RiotNSE/NotificationService.swift
+++ RiotNSE/NotificationService.swift
@@ -168,14 +168,6 @@ class NotificationService: UNNotificationServiceExtension {
             }
             
             //  encrypted
-            if !self.showDecryptedContentInNotifications {
-                //  do not show decrypted content in notification
-                NSLog("[NotificationService] fetchEvent: Do not show decrypted content in notifications, no need to attempt to decrypt it.")
-                self.processEvent(event)
-                return
-            }
-            
-            //  should show decrypted content in notification
             if event.clear != nil {
                 //  already decrypted
                 NSLog("[NotificationService] fetchEvent: Event already decrypted.")
@@ -329,6 +321,20 @@ class NotificationService: UNNotificationServiceExtension {
             let pushRule = session.notificationCenter.rule(matching: event, roomState: roomState)
             
             switch event.eventType {
+            case .callInvite:
+                let offer = event.content["offer"] as? [AnyHashable: Any]
+                let sdp = offer?["sdp"] as? String
+                let isVideoCall = sdp?.contains("m=video") ?? false
+                
+                if isVideoCall {
+                    notificationBody = NSString.localizedUserNotificationString(forKey: "VIDEO_CALL_FROM_USER", arguments: [eventSenderName as Any])
+                } else {
+                    notificationBody = NSString.localizedUserNotificationString(forKey: "VOICE_CALL_FROM_USER", arguments: [eventSenderName as Any])
+                }
+                
+                // call notifications should stand out from normal messages, so we don't stack them
+                threadIdentifier = nil
+                self.sendVoipPush(forEvent: event)
             case .roomMessage, .roomEncrypted:
                 if room.isMentionsOnly {
                     // A local notification will be displayed only for highlighted notification.
@@ -423,20 +429,6 @@ class NotificationService: UNNotificationServiceExtension {
                     }
                 }
                 break
-            case .callInvite:
-                let offer = event.content["offer"] as? [AnyHashable: Any]
-                let sdp = offer?["sdp"] as? String
-                let isVideoCall = sdp?.contains("m=video") ?? false
-                
-                if isVideoCall {
-                    notificationBody = NSString.localizedUserNotificationString(forKey: "VIDEO_CALL_FROM_USER", arguments: [eventSenderName as Any])
-                } else {
-                    notificationBody = NSString.localizedUserNotificationString(forKey: "VOICE_CALL_FROM_USER", arguments: [eventSenderName as Any])
-                }
-                
-                // call notifications should stand out from normal messages, so we don't stack them
-                threadIdentifier = nil
-                self.sendVoipPush(forEvent: event)
             case .roomMember:
                 let roomDisplayName = room.summary.displayname
                 
@@ -554,15 +546,19 @@ class NotificationService: UNNotificationServiceExtension {
         return "QUICK_REPLY"
     }
     
+    /// Attempts to send trigger a VoIP push for the given event
+    /// - Parameter event: The call invite event.
     private func sendVoipPush(forEvent event: MXEvent) {
-        guard let token = pushNotificationManager.pushToken else {
+        guard let token = pushNotificationManager.pushKitToken else {
             return
         }
         
+        pushNotificationManager.lastCallInvite = event
+        
         let appId = BuildSettings.pushKitAppId
         
-        pushGatewayRestClient.notifyApp(withId: appId, pushToken: token, eventId: event.eventId, roomId: event.roomId, eventType: event.wireType, sender: event.sender, success: { (rejected) in
-            NSLog("[NotificationService] sendVoipPush succeeded, rejected token: \(rejected)")
+        pushGatewayRestClient.notifyApp(withId: appId, pushToken: token, eventId: event.eventId, roomId: event.roomId, eventType: nil, sender: event.sender, success: { (rejected) in
+            NSLog("[NotificationService] sendVoipPush succeeded, rejected tokens: \(rejected)")
         }) { (error) in
             NSLog("[NotificationService] sendVoipPush failed with error: \(error)")
         }
