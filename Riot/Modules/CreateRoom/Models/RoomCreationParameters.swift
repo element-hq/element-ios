1c5bb22a8ddce4e353fa477c8b5163da8c737e3d
ismailgulek <ismailg@matrix.org>
2020-09-11 13:35:41 +0300

    Update room creation params

--- Riot/Modules/CreateRoom/Models/RoomCreationParameters.swift
+++ Riot/Modules/CreateRoom/Models/RoomCreationParameters.swift
@@ -20,8 +20,21 @@ struct RoomCreationParameters {
     var name: String?
     var topic: String?
     var address: String?
-    var avatarImage: UIImage?
+    var avatarImage: UIImage? {
+        return userSelectedAvatar ?? initialsAvatar
+    }
     var isEncrypted: Bool = false
-    var isPublic: Bool = false
+    var isPublic: Bool = false {
+        didSet {
+            if !isPublic {
+                //  if set to private again, reset some fields
+                showInDirectory = false
+                address = nil
+            }
+        }
+    }
     var showInDirectory: Bool = false
+    
+    var userSelectedAvatar: UIImage?
+    var initialsAvatar: UIImage?
 }
