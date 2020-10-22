66b24716dfcda87771b0d95ecf1bc52f62e9c3b7
ismailgulek <ismailg@matrix.org>
2020-10-22 17:26:25 +0300

    Rever background sync method

--- Riot/Managers/PushNotification/PushNotificationService.m
+++ Riot/Managers/PushNotification/PushNotificationService.m
@@ -305,7 +305,7 @@ Matrix session observer used to detect new opened sessions.
             NSLog(@"[PushNotificationService] launchBackgroundSync");
             MXWeakify(self);
 
-            [account backgroundSync:20000 success:^(MXSyncResponse *syncResponse) {
+            [account backgroundSync:20000 success:^{
                 
                 // Sanity check
                 MXStrongifyAndReturnIfNil(self);
