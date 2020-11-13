2865f7cbdff091f5fbe2765ca470ef8129aa3a8a
SBiOSoftWhare <steveb@matrix.org>
2020-11-13 09:47:39 +0100

    Fix conflict.

--- Riot/Modules/People/Views/InviteRecentTableViewCell.m
+++ Riot/Modules/People/Views/InviteRecentTableViewCell.m
@@ -26,6 +26,7 @@
 
 #pragma mark - Constant definitions
 
+NSString *const kInviteRecentTableViewCellAcceptButtonPressed = @"kInviteRecentTableViewCellAcceptButtonPressed";
 NSString *const kInviteRecentTableViewCellPreviewButtonPressed = @"kInviteRecentTableViewCellPreviewButtonPressed";
 NSString *const kInviteRecentTableViewCellDeclineButtonPressed = @"kInviteRecentTableViewCellDeclineButtonPressed";
 
@@ -42,14 +43,12 @@ NSString *const kInviteRecentTableViewCellRoomKey = @"kInviteRecentTableViewCell
     [self.leftButton.layer setCornerRadius:5];
     self.leftButton.clipsToBounds = YES;
     [self.leftButton setTitle:NSLocalizedStringFromTable(@"decline", @"Vector", nil) forState:UIControlStateNormal];
-    [self.leftButton setTitle:NSLocalizedStringFromTable(@"decline", @"Vector", nil) forState:UIControlStateHighlighted];
     [self.leftButton addTarget:self action:@selector(onDeclinePressed:) forControlEvents:UIControlEventTouchUpInside];
     
     [self.rightButton.layer setCornerRadius:5];
     self.rightButton.clipsToBounds = YES;
-    [self.rightButton setTitle:NSLocalizedStringFromTable(@"preview", @"Vector", nil) forState:UIControlStateNormal];
-    [self.rightButton setTitle:NSLocalizedStringFromTable(@"preview", @"Vector", nil) forState:UIControlStateHighlighted];
-    [self.rightButton addTarget:self action:@selector(onPreviewPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.rightButton setTitle:NSLocalizedStringFromTable(@"accept", @"Vector", nil) forState:UIControlStateNormal];
+    [self.rightButton addTarget:self action:@selector(onAcceptPressed:) forControlEvents:UIControlEventTouchUpInside];
     
     [self.noticeBadgeView.layer setCornerRadius:10];
     
@@ -66,6 +65,13 @@ NSString *const kInviteRecentTableViewCellRoomKey = @"kInviteRecentTableViewCell
     self.noticeBadgeView.backgroundColor = ThemeService.shared.theme.noticeColor;
 }
 
+- (void)prepareForReuse
+{
+    [super prepareForReuse];
+    
+    [self resetButtonViews];
+}
+
 - (void)onDeclinePressed:(id)sender
 {
     if (self.delegate)
@@ -79,7 +85,7 @@ NSString *const kInviteRecentTableViewCellRoomKey = @"kInviteRecentTableViewCell
     }
 }
 
-- (void)onPreviewPressed:(id)sender
+- (void)onAcceptPressed:(id)sender
 {
     if (self.delegate)
     {
@@ -87,7 +93,7 @@ NSString *const kInviteRecentTableViewCellRoomKey = @"kInviteRecentTableViewCell
         
         if (room)
         {
-            [self.delegate cell:self didRecognizeAction:kInviteRecentTableViewCellPreviewButtonPressed userInfo:@{kInviteRecentTableViewCellRoomKey:room}];
+            [self.delegate cell:self didRecognizeAction:kInviteRecentTableViewCellAcceptButtonPressed userInfo:@{kInviteRecentTableViewCellRoomKey:room}];
         }
     }
 }
@@ -95,6 +101,13 @@ NSString *const kInviteRecentTableViewCellRoomKey = @"kInviteRecentTableViewCell
 - (void)render:(MXKCellData *)cellData
 {
     [super render:cellData];
+        
+    MXRoom *room = roomCellData.roomSummary.room;
+    
+    if (room.roomId)
+    {
+        [self updateButtonViewsWith:room.roomId];
+    }
 }
 
 + (CGFloat)heightForCellData:(MXKCellData *)cellData withMaximumWidth:(CGFloat)maxWidth
