db503d2be1acddf69462ebc6df82459fbeedd706
Gil Eluard <gil.eluard@gmail.com>
2021-01-05 09:44:07 +0100

    Updated fastlane file to launch xcodegen

--- createWorkspace.sh
+++ createWorkspace.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cp IDETemplateMacros.plist Riot.xcodeproj/xcshareddata/
-if [ $(gem list bundler -i) ]; then
-	bundle install
-	bundle exec pod install
-else
-	pod install
-fi
+# if [ $(gem list bundler -i) ]; then
+# 	bundle install
+# 	bundle exec pod install
+# else
+# 	pod install
+# fi
