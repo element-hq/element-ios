15917907b7143da3881299bf93b9b2c0bde9d65f
langleyd <davidl@element.io>
2021-07-06 13:14:15 +0100

    Enabled new room settings  UI while I do ad hoc build

--- Config/BuildSettings.swift
+++ Config/BuildSettings.swift
@@ -295,7 +295,7 @@ final class BuildSettings: NSObject {
     static let roomSettingsScreenShowFlairSettings: Bool = true
     static let roomSettingsScreenShowAdvancedSettings: Bool = true
     static let roomSettingsScreenAdvancedShowEncryptToVerifiedOption: Bool = true
-    static let roomSettingsScreenShowNotificationsV2: Bool = false
+    static let roomSettingsScreenShowNotificationsV2: Bool = true
 
     // MARK: - Room Member Screen
     
