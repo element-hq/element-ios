d26a3c7d95bb41dcacfd8e15fc6c272c8bc09221
SBiOSoftWhare <steveb@matrix.org>
2021-07-30 17:28:35 +0200

    App variants: Add script to choose an app variant from "Variants" folder.

---
+++ Variants/setup_app_variant.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# If you want to use an existing build variant or your own one you have to choose or create a folder with your variant name in "Variants" folder.
+
+if [ ! $# -eq 1 ]; then
+    echo "Usage: ./setup_app_variant.sh [variant folder name]"
+    exit 1
+fi
+
+# Get variant name from user input
+VARIANT_NAME=$1
+VARIANTS_ROOT_FOLDER="Variants/"
+
+cd ..
+
+# Get the variant root folder
+variant_root_folder="${VARIANTS_ROOT_FOLDER}${VARIANT_NAME}"
+
+# Check if variant folder exists
+if [ ! -d "$variant_root_folder" ]; then
+    echo "Variant folder: ${variant_root_folder} do not exists"
+    exit 1
+fi
+
+echo "Applying app variant: ${VARIANT_NAME}"
+
+# Replace build identifiers and provisioning profiles
+cp -f "${variant_root_folder}/AppIdentifiers.xcconfig" Config/AppIdentifiers.xcconfig
+
+# Replace app icon
+rsync -a "${variant_root_folder}/AppIcon.appiconset/" Riot/Assets/SharedImages.xcassets/AppIcon.appiconset/
