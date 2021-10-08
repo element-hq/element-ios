73919b17916a17dd34899fe426b8a7e6c8d05010
SBiOSoftWhare <steveb@matrix.org>
2021-10-08 15:45:53 +0200

    Array: Update convenient method signature.

--- Riot/Categories/Array.swift
+++ Riot/Categories/Array.swift
@@ -20,7 +20,7 @@ extension Array where Element: Equatable {
 
 /// Remove first collection element that is equal to the given `object`
 /// Credits: https://stackoverflow.com/a/45008042
-    mutating func vc_remove(object: Element) {
+    mutating func vc_removeFirstOccurrence(of object: Element) {
         guard let index = firstIndex(of: object) else {
             return
         }
