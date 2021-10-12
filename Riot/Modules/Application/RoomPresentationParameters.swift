bb6283554f95a2f8a1bb61161cbe27f8578eb5c9
SBiOSoftWhare <steveb@matrix.org>
2021-10-12 19:09:18 +0200

    Add RoomPresentationParameters and RoomPreviewPresentationParameters to display a room or room preview with identifiers and presentation configuration.

---
+++ Riot/Modules/Application/RoomPresentationParameters.swift
@@ -0,0 +1,56 @@
+// 
+// Copyright 2021 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+/// Presentation parameters to display a room with a provided identifier in a specific matrix session.
+@objcMembers
+class RoomPresentationParameters: NSObject {
+    
+    // MARK: - Properties
+
+    /// The room identifier
+    let roomId: String
+    
+    /// If not nil, the room will be opened on this event.
+    let eventId: String?
+    
+    /// The Matrix session in which the room should be available.
+    let mxSession: MXSession
+    
+    /// Indicate to pop to home and restore initial view hierarchy
+    let restoreInitialDisplay: Bool
+    
+    /// Indicate to stack above visible views
+    /// If this variable is set to true `restoreInitialDisplay` should be set to false to have effect
+    let stackAboveVisibleViews: Bool
+    
+    // MARK: - Setup
+    
+    init(roomId: String,
+         eventId: String?,
+         mxSession: MXSession,
+         restoreInitialDisplay: Bool,
+         stackAboveVisibleViews: Bool) {
+        self.roomId = roomId
+        self.eventId = eventId
+        self.mxSession = mxSession
+        self.restoreInitialDisplay = restoreInitialDisplay
+        self.stackAboveVisibleViews = stackAboveVisibleViews
+        
+        super.init()
+    }
+}
