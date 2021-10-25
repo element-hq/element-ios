997ebbee03f792403f7848b6955a8c714d3e1dea
Stefan Ceriu <stefanc@matrix.org>
2021-10-25 17:49:35 +0300

    vector-im/element-ios/issues/4384 - Using mutable room list fetch sort options after chaning them to be a structure.

--- Riot/Modules/Common/Recents/Service/MatrixSDK/RecentsListService.swift
+++ Riot/Modules/Common/Recents/Service/MatrixSDK/RecentsListService.swift
@@ -440,7 +440,7 @@ public class RecentsListService: NSObject, RecentsListServiceProtocol {
         case .home:
             fetcher.fetchOptions.sortOptions = sortOptions
         case .favourites:
-            let newSortOptions = sortOptions
+            var newSortOptions = sortOptions
             newSortOptions.favoriteTag = true
             fetcher.fetchOptions.sortOptions = newSortOptions
         default:
