f372e23e58bfc8e2a28dab0210d4cd0d692e40c1
David Langley <langley.dave@gmail.com>
2021-11-04 16:22:58 +0000

    Update matrixPermalinkPaths -> permalinkSupportedHosts for consistency with android naming

--- Config/BuildSettings.swift
+++ Config/BuildSettings.swift
@@ -118,9 +118,9 @@ final class BuildSettings: NSObject {
     "https://element.io/help"
     
     
-    // MARk: - Matrix permalinks
-    // Paths for URLs that will considered as Matrix permalinks. Those permalinks are opened within the app
-    static let matrixPermalinkPaths: [String: [String]] = [
+    // MARK: - Permalinks
+    // Hosts/Paths for URLs that will considered as valid permalinks. Those permalinks are opened within the app.
+    static let permalinkSupportedHosts: [String: [String]] = [
         "app.element.io": [],
         "staging.element.io": [],
         "develop.element.io": [],
@@ -142,7 +142,7 @@ final class BuildSettings: NSObject {
     // This baseURL is used to generate permalinks within the app (E.g. timeline message permalinks).
     // Optional String that when set is used as permalink base, when nil matrix.to format is used.
     // Example value would be "https://www.example.com", note there is no trailing '/'.
-    static let clientPermalinkBaseUrl: String? = nil
+    static let clientPermalinkBaseUrl: String? = "https://app.element.io"
     
     // MARK: - VoIP
     static var allowVoIPUsage: Bool {
