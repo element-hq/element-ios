b595b3a3a55ce41a3b89785f1695d276cd7500e1
SBiOSoftWhare <steveb@matrix.org>
2021-12-01 11:18:19 +0100

    Handle MatrixKit tests new location, update bridging header.

--- RiotTests/MatrixKitTests/MatrixKitTests-Bridging-Header.h
+++ RiotTests/MatrixKitTests/MatrixKitTests-Bridging-Header.h
@@ -2,5 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "Riot-Bridging-Header.h"
-#import "MatrixKitTests-Bridging-Header.h"
+#import "MXKRoomDataSource+Tests.h"
