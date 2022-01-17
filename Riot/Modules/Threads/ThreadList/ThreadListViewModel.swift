d7ed2d3b09e81c09671db849b10f7e31a87e796f
ismailgulek <ismailg@matrix.org>
2022-01-17 16:33:02 +0300

    Fix event content where the thread root event is a reply event

--- Riot/Modules/Threads/ThreadList/ThreadListViewModel.swift
+++ Riot/Modules/Threads/ThreadList/ThreadListViewModel.swift
@@ -204,6 +204,23 @@ final class ThreadListViewModel: ThreadListViewModelProtocol {
         guard let message = thread.rootMessage else {
             return nil
         }
+        if message.isReply(), let newMessage = message.copy() as? MXEvent {
+            var jsonDict = newMessage.isEncrypted ? newMessage.clear?.jsonDictionary() : newMessage.jsonDictionary()
+            if var content = jsonDict?["content"] as? [String: Any] {
+                content.removeValue(forKey: "format")
+                content.removeValue(forKey: "formatted_body")
+                content.removeValue(forKey: kMXEventRelationRelatesToKey)
+                if let replyText = MXReplyEventParser().parse(newMessage)?.bodyParts.replyText {
+                    content["body"] = replyText
+                }
+                jsonDict?["content"] = content
+            }
+            let trimmedMessage = MXEvent(fromJSON: jsonDict)
+            let formatterError = UnsafeMutablePointer<MXKEventFormatterError>.allocate(capacity: 1)
+            return eventFormatter.attributedString(from: trimmedMessage,
+                                                   with: roomState,
+                                                   error: formatterError)
+        }
         let formatterError = UnsafeMutablePointer<MXKEventFormatterError>.allocate(capacity: 1)
         return eventFormatter.attributedString(from: message,
                                                with: roomState,
