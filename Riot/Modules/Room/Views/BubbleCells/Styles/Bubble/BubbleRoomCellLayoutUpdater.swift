99461bd8844b23f3e21dc031c042ae3c83f5fe1f
SBiOSoftWhare <steveb@matrix.org>
2022-01-24 11:14:30 +0100

    Message bubbles: Handle emotes.

--- Riot/Modules/Room/Views/BubbleCells/Styles/Bubble/BubbleRoomCellLayoutUpdater.swift
+++ Riot/Modules/Room/Views/BubbleCells/Styles/Bubble/BubbleRoomCellLayoutUpdater.swift
@@ -165,7 +165,7 @@ class BubbleRoomCellLayoutUpdater: RoomCellLayoutUpdating {
                 let messageType = MXMessageType(identifier: messageTypeString)
                 
                 switch messageType {
-                case .text, .file:
+                case .text, .emote, .file:
                     return true
                 default:
                     break
