ddc632f844ca82b304ec7fae36b28a987d704143
SBiOSoftWhare <steveb@matrix.org>
2022-02-15 12:03:08 +0100

    BubbleRoomCellLayoutConstants: Update constants and add new ones.

--- Riot/Modules/Room/Views/BubbleCells/Styles/Bubble/BubbleRoomCellLayoutConstants.swift
+++ Riot/Modules/Room/Views/BubbleCells/Styles/Bubble/BubbleRoomCellLayoutConstants.swift
@@ -21,9 +21,26 @@ import Foundation
 @objcMembers
 final class BubbleRoomCellLayoutConstants: NSObject {
     
+    /// Inner content view margins
+    static let innerContentViewMargins: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 5.0, right: 0)
+    
+    // Text message bubbles margins from cell content view
+    
     static let outgoingBubbleBackgroundMargins: UIEdgeInsets = UIEdgeInsets(top: 0, left: 80, bottom: 0, right: 34)
 
-    static let incomingBubbleBackgroundMargins: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 80)
+    static let incomingBubbleBackgroundMargins: UIEdgeInsets = UIEdgeInsets(top: 0, left: 48, bottom: 0, right: 80)
+    
+    static let bubbleTextViewInsets: UIEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 45)
+    
+    // Voice message
+    
+    static let voiceMessagePlaybackViewRightMargin: CGFloat = 40
+    
+    // Polls
+    
+    static let pollBubbleBackgroundInsets: UIEdgeInsets = UIEdgeInsets(top: 2, left: 10, bottom: 0, right: 10)
+
+    // Decoration margins
     
     static let threadSummaryViewMargins: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 5, right: 0)
 }
