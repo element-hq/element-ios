bf37be670e8dd41c9edff0f278d0039c9adb50e2
Stefan Ceriu <stefanc@matrix.org>
2022-02-28 09:34:37 +0200

    Added swiftgen step for generating strings that won't be exposed to Weblate but still part of VectorL10n.

---
+++ Riot/Generated/UntranslatedStrings.swift
@@ -0,0 +1,19 @@
+// swiftlint:disable all
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Strings
+
+// swiftlint:disable function_parameter_count identifier_name line_length type_body_length
+public extension VectorL10n {
+  /// Untranslated test
+  static var untranslatedTest: String { 
+    return VectorL10n.tr("Untranslated", "Untranslated test") 
+  }
+}
+// swiftlint:enable function_parameter_count identifier_name line_length type_body_length
+
