c165bd55312d44255df1e3419a88027cf7a6418c
aringenbach <arnaudr@element.io>
2022-04-04 15:44:12 +0200

    Display presence in RoomInfoBasicView

--- Riot/Modules/Room/RoomInfo/RoomInfoList/RoomInfoListViewModel.swift
+++ Riot/Modules/Room/RoomInfo/RoomInfoList/RoomInfoListViewModel.swift
@@ -34,6 +34,7 @@ final class RoomInfoListViewModel: NSObject, RoomInfoListViewModelType {
     
     private var viewData: RoomInfoListViewData {
         let encryptionImage = EncryptionTrustLevelBadgeImageHelper.roomBadgeImage(for: room.summary.roomEncryptionTrustLevel())
+        let directUserPresence = session.user(withUserId: room.directUserId)?.presence ?? MXPresenceUnknown
         
         let basicInfoViewData = RoomInfoBasicViewData(avatarUrl: room.summary.avatar,
                                                       mediaManager: session.mediaManager,
@@ -43,7 +44,8 @@ final class RoomInfoListViewModel: NSObject, RoomInfoListViewModelType {
                                                       roomTopic: room.summary.topic,
                                                       encryptionImage: encryptionImage,
                                                       isEncrypted: room.summary.isEncrypted,
-                                                      isDirect: room.isDirect)
+                                                      isDirect: room.isDirect,
+                                                      directUserPresence: directUserPresence)
         
         return RoomInfoListViewData(numberOfMembers: Int(room.summary.membersCount.joined),
                                     basicInfoViewData: basicInfoViewData)
