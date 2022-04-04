8ee55203d89b5f63c2c532c17b2db152c142949a
aringenbach <arnaudr@element.io>
2022-04-04 16:50:14 +0200

    Add observer to refresh RoomTitleView on presence updates

--- Riot/Modules/Room/Views/Title/RoomTitleView.m
+++ Riot/Modules/Room/Views/Title/RoomTitleView.m
@@ -20,6 +20,15 @@
 #import "ThemeService.h"
 #import "GeneratedInterface-Swift.h"
 
+@interface RoomTitleView()
+{
+    /**
+     The observer of the presence for direct user.
+    */
+    id mxDirectUserPresenceObserver;
+}
+@end
+
 @implementation RoomTitleView
 
 + (UINib *)nib
@@ -109,6 +118,22 @@
     }
     else if (self.mxRoom)
     {
+        if (!mxDirectUserPresenceObserver && self.mxRoom.isDirect)
+        {
+            // Observe contact presence change
+            mxDirectUserPresenceObserver = [[NSNotificationCenter defaultCenter] addObserverForName:kMXKContactManagerMatrixUserPresenceChangeNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
+                
+                NSString* directUserId = self.mxRoom.directUserId;
+                
+                if (directUserId && [directUserId isEqualToString:notif.object])
+                {
+                    [self refreshContactPresence];
+                }
+            }];
+            
+            [self refreshContactPresence];
+        }
+        
         self.displayNameTextField.text = self.mxRoom.summary.displayname;
         if (!self.displayNameTextField.text.length)
         {
@@ -119,32 +144,40 @@
         {
             self.displayNameTextField.textColor = ThemeService.shared.theme.textPrimaryColor;
         }
-        
-        MXUser *contact = [self.mxRoom.mxSession userWithUserId:self.mxRoom.directUserId];
-        BOOL presenceHidden = contact.presence == MXPresenceUnknown;
-        self.presenceIndicatorView.hidden = presenceHidden;
-        self.presenceIndicatorView.borderColor = ThemeService.shared.theme.headerBackgroundColor;
-        self.presenceIndicatorView.presence = contact.presence;
-        if (presenceHidden)
-        {
-            [self.badgeImageViewLeadingToPictureViewConstraint setPriority:UILayoutPriorityDefaultLow];
-            [self.badgeImageViewCenterYToDisplayNameConstraint setPriority:UILayoutPriorityDefaultLow];
-            [self.badgeImageViewToPictureViewBottomConstraint setPriority:UILayoutPriorityRequired];
-            [self.badgeImageViewToPictureViewTrailingConstraint setPriority:UILayoutPriorityRequired];
-        }
-        else
-        {
-            [self.badgeImageViewToPictureViewBottomConstraint setPriority:UILayoutPriorityDefaultLow];
-            [self.badgeImageViewToPictureViewTrailingConstraint setPriority:UILayoutPriorityDefaultLow];
-            [self.badgeImageViewLeadingToPictureViewConstraint setPriority:UILayoutPriorityRequired];
-            [self.badgeImageViewCenterYToDisplayNameConstraint setPriority:UILayoutPriorityRequired];
-        }
+    }
+}
+
+- (void)refreshContactPresence
+{
+    MXUser *contact = [self.mxRoom.mxSession userWithUserId:self.mxRoom.directUserId];
+    BOOL presenceHidden = contact.presence == MXPresenceUnknown;
+    self.presenceIndicatorView.hidden = presenceHidden;
+    self.presenceIndicatorView.borderColor = ThemeService.shared.theme.headerBackgroundColor;
+    self.presenceIndicatorView.presence = contact.presence;
+    if (presenceHidden)
+    {
+        [self.badgeImageViewLeadingToPictureViewConstraint setPriority:UILayoutPriorityDefaultLow];
+        [self.badgeImageViewCenterYToDisplayNameConstraint setPriority:UILayoutPriorityDefaultLow];
+        [self.badgeImageViewToPictureViewBottomConstraint setPriority:UILayoutPriorityRequired];
+        [self.badgeImageViewToPictureViewTrailingConstraint setPriority:UILayoutPriorityRequired];
+    }
+    else
+    {
+        [self.badgeImageViewToPictureViewBottomConstraint setPriority:UILayoutPriorityDefaultLow];
+        [self.badgeImageViewToPictureViewTrailingConstraint setPriority:UILayoutPriorityDefaultLow];
+        [self.badgeImageViewLeadingToPictureViewConstraint setPriority:UILayoutPriorityRequired];
+        [self.badgeImageViewCenterYToDisplayNameConstraint setPriority:UILayoutPriorityRequired];
     }
 }
 
 - (void)destroy
 {
     self.tapGestureDelegate = nil;
+    
+    if (mxDirectUserPresenceObserver) {
+        [[NSNotificationCenter defaultCenter] removeObserver:mxDirectUserPresenceObserver];
+        mxDirectUserPresenceObserver = nil;
+    }
     
     [super destroy];
 }
