c04db50a6c5437b22236057189e707b0a959397e
Doug <douglase@element.io>
2022-04-13 10:06:43 +0100

    Fix some warnings.

--- Riot/Modules/Threads/Beta/ThreadsBetaCoordinatorBridgePresenter.swift
+++ Riot/Modules/Threads/Beta/ThreadsBetaCoordinatorBridgePresenter.swift
@@ -26,7 +26,8 @@ import MatrixSDK
 
 /// ThreadsBetaCoordinatorBridgePresenter enables to start ThreadsBetaCoordinator from a view controller.
 /// This bridge is used while waiting for global usage of coordinator pattern.
-/// **WARNING**: This class breaks the Coordinator abstraction and it has been introduced for **Objective-C compatibility only** (mainly for integration in legacy view controllers). Each bridge should be removed once the underlying Coordinator has been integrated by another Coordinator.
+/// **WARNING**: This class breaks the Coordinator abstraction and it has been introduced for **Objective-C compatibility only** (mainly for integration in legacy view controllers).
+/// Each bridge should be removed once the underlying Coordinator has been integrated by another Coordinator.
 @objcMembers
 final class ThreadsBetaCoordinatorBridgePresenter: NSObject {
     
