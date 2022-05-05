d1446b465bd617c069889c310b445aa8c06ea7af
Doug <douglase@element.io>
2022-05-05 13:39:56 +0100

    Fix unit tests.

--- RiotSwiftUI/Modules/Authentication/VerifyEmail/Test/Unit/AuthenticationVerifyEmailViewModelTests.swift
+++ RiotSwiftUI/Modules/Authentication/VerifyEmail/Test/Unit/AuthenticationVerifyEmailViewModelTests.swift
@@ -32,12 +32,13 @@ class AuthenticationVerifyEmailViewModelTests: XCTestCase {
         context = viewModel.context
     }
 
-    func testSentEmailState() {
+    func testSentEmailState() async {
         // Given a view model where the user hasn't yet sent the verification email.
         XCTAssertFalse(context.viewState.hasSentEmail, "The view model should start with hasSentEmail equal to false")
         
         // When updating to indicate that an email has been send.
-        viewModel.updateForSentEmail()
+        let task = Task { await viewModel.updateForSentEmail() }
+        _ = await task.result
         
         // Then the view model should update to reflect a sent email.
         XCTAssertTrue(context.viewState.hasSentEmail, "The view model should start with hasSentEmail equal to false")
