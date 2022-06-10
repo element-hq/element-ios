44d54710ed311073f8d0e107fb14c36be72069ee
ismailgulek <ismailg@matrix.org>
2022-06-10 18:33:50 +0300

    Fix tests

--- RiotSwiftUI/Modules/Settings/ChangePassword/Test/Unit/ChangePasswordViewModelTests.swift
+++ RiotSwiftUI/Modules/Settings/ChangePassword/Test/Unit/ChangePasswordViewModelTests.swift
@@ -29,14 +29,14 @@ class ChangePasswordViewModelTests: XCTestCase {
         XCTAssert(context.newPassword1.isEmpty, "The view model should start with an empty new password 1.")
         XCTAssert(context.newPassword2.isEmpty, "The view model should start with an empty new password 2.")
         XCTAssertFalse(context.viewState.canSubmit, "The view model should not be able to submit.")
-        XCTAssertTrue(context.signoutAllDevices, "The view model should start with sign out of all devices checked.")
+        XCTAssertFalse(context.signoutAllDevices, "The view model should start with sign out of all devices unchecked.")
     }
 
     @MainActor func testValidState() async {
         let viewModel = ChangePasswordViewModel(oldPassword: "12345678",
                                                 newPassword1: "87654321",
                                                 newPassword2: "87654321",
-                                                signoutAllDevices: false)
+                                                signoutAllDevices: true)
         let context = viewModel.context
 
         // Given a filled view model in valid state
@@ -44,7 +44,7 @@ class ChangePasswordViewModelTests: XCTestCase {
         XCTAssertFalse(context.newPassword1.isEmpty, "The view model should start with an empty new password 1.")
         XCTAssertFalse(context.newPassword2.isEmpty, "The view model should start with an empty new password 2.")
         XCTAssertTrue(context.viewState.canSubmit, "The view model should be able to submit.")
-        XCTAssertFalse(context.signoutAllDevices, "Sign out of all devices should be unchecked.")
+        XCTAssertTrue(context.signoutAllDevices, "Sign out of all devices should be checked.")
     }
 
 }
