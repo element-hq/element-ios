308b49554ad8dbbc3b2d0a105c822038169268b1
Gil Eluard <gile@element.io>
2022-06-20 14:30:01 +0200

    the app doesn't allow to add a DM to a space, only rooms
    - Initial fix

--- RiotSwiftUI/Modules/Spaces/MatrixItemChooser/Service/MatrixSDK/MatrixItemChooserRoomsDataSource.swift
+++ RiotSwiftUI/Modules/Spaces/MatrixItemChooser/Service/MatrixSDK/MatrixItemChooserRoomsDataSource.swift
@@ -20,16 +20,47 @@ class MatrixItemChooserRoomsDataSource: MatrixItemChooserDataSource {
     var preselectedItemIds: Set<String>? { nil }
 
     func sections(with session: MXSession, completion: @escaping (Result<[MatrixListItemSectionData], Error>) -> Void) {
-        completion(Result(catching: {
-            [
-                MatrixListItemSectionData(items: session.rooms.compactMap { room in
-                    if room.summary.roomType == .space || room.isDirect {
-                        return nil
-                    }
-                    
-                    return MatrixListItemData(mxRoom: room, spaceService: session.spaceService)
-                })
-            ]
-        }))
+        var favouriteRooms: [MatrixListItemData] = []
+        var peopleRooms: [MatrixListItemData] = []
+        var rooms: [MatrixListItemData] = []
+        
+        for room in session.rooms where room.summary.roomType != .space {
+            let currentTag = room.accountData.tags?.values.first
+            if currentTag?.name == kMXRoomTagFavourite {
+                favouriteRooms.append(MatrixListItemData(mxRoom: room, spaceService: session.spaceService))
+            } else if room.summary.isDirect {
+                peopleRooms.append(MatrixListItemData(mxRoom: room, spaceService: session.spaceService))
+            } else {
+                rooms.append(MatrixListItemData(mxRoom: room, spaceService: session.spaceService))
+            }
+        }
+        
+        favouriteRooms = sortedArray(favouriteRooms)
+        peopleRooms = sortedArray(peopleRooms)
+        rooms = sortedArray(rooms)
+        
+        var sections: [MatrixListItemSectionData] = []
+        if (!favouriteRooms.isEmpty) {
+            sections.append(MatrixListItemSectionData(title: VectorL10n.titleFavourites, items: favouriteRooms))
+        }
+        if (!peopleRooms.isEmpty) {
+            sections.append(MatrixListItemSectionData(title: VectorL10n.titlePeople, items: peopleRooms))
+        }
+        if (!rooms.isEmpty) {
+            sections.append(MatrixListItemSectionData(title: VectorL10n.titlePeople, items: rooms))
+        }
+
+        completion(Result(catching: { sections }))
+    }
+    
+    // MARK : - Private
+    
+    private func sortedArray(_ array: [MatrixListItemData]) -> [MatrixListItemData] {
+        return array.sorted { item1, item2 in
+            guard let displayName1 = item1.displayName, let displayName2 = item2.displayName else {
+                return true
+            }
+            return displayName1.lowercased() <= displayName2.lowercased()
+        }
     }
 }
