4507b66391cddb8cf6de96db8319f4bc3c64f871
wtimme <wtimme@users.noreply.github.com>
2022-06-30 21:09:37 +0200

    Move the implementation of `INSendMessageIntentHandling` to a dedicated class (#6203)

--- SiriIntents/IntentHandler.m
+++ SiriIntents/IntentHandler.m
@@ -21,21 +21,16 @@
 #import "ContactResolver.h"
 #import "StartAudioCallIntentHandler.h"
 #import "StartVideoCallIntentHandler.h"
+#import "SendMessageIntentHandler.h"
 
 #if __has_include(<MatrixSDK/MXJingleCallStack.h>)
 #define CALL_STACK_JINGLE
 #endif
 
-@interface IntentHandler () <INSendMessageIntentHandling>
+@interface IntentHandler ()
 
 // Build Settings
 @property (nonatomic) id<Configurable> configuration;
-
-/**
- The room that is currently being used to send a message. This is to ensure a
- strong ref is maintained on the `MXRoom` until sending has completed.
- */
-@property (nonatomic) MXRoom *selectedRoom;
 
 @property (nonatomic) id<ContactResolving> contactResolver;
 @property (nonatomic) id<INStartAudioCallIntentHandling> startAudioCallIntentHandler;
@@ -54,7 +49,7 @@
         _contactResolver = [[ContactResolver alloc] init];
         _startAudioCallIntentHandler = [[StartAudioCallIntentHandler alloc] init];
         _startVideoCallIntentHandler = [[StartVideoCallIntentHandler alloc] init];
-        _sendMessageIntentHandler = self;
+        _sendMessageIntentHandler = [[SendMessageIntentHandler alloc] init];
         
         // Set static application settings
         _configuration = [CommonConfiguration new];
@@ -93,122 +88,6 @@
     }
     
     return self;
-}
-
-#pragma mark - INSendMessageIntentHandling
-
-- (void)resolveRecipientsForSendMessage:(INSendMessageIntent *)intent completion:(void (^)(NSArray<INSendMessageRecipientResolutionResult *> * _Nonnull))completion
-{
-    [self.contactResolver resolveContacts:intent.recipients withCompletion:completion];
-}
-
-- (void)resolveContentForSendMessage:(INSendMessageIntent *)intent withCompletion:(void (^)(INStringResolutionResult * _Nonnull))completion
-{
-    NSString *message = intent.content;
-    if (message && ![message isEqualToString:@""])
-        completion([INStringResolutionResult successWithResolvedString:message]);
-    else
-        completion([INStringResolutionResult needsValue]);
-}
-
-- (void)confirmSendMessage:(INSendMessageIntent *)intent completion:(void (^)(INSendMessageIntentResponse * _Nonnull))completion
-{
-    INSendMessageIntentResponse *response = nil;
-    
-    MXKAccount *account = [MXKAccountManager sharedManager].activeAccounts.firstObject;
-    if (account)
-    {
-        NSUserActivity *userActivity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass([INSendMessageIntent class])];
-        response = [[INSendMessageIntentResponse alloc] initWithCode:INSendMessageIntentResponseCodeReady userActivity:userActivity];
-    }
-    else
-    {
-        // User hasn't logged in
-        response = [[INSendMessageIntentResponse alloc] initWithCode:INSendMessageIntentResponseCodeFailureRequiringAppLaunch userActivity:nil];
-    }
-    
-    completion(response);
-}
-
-- (void)handleSendMessage:(INSendMessageIntent *)intent completion:(void (^)(INSendMessageIntentResponse * _Nonnull))completion
-{
-    void (^completeWithCode)(INSendMessageIntentResponseCode) = ^(INSendMessageIntentResponseCode code) {
-        NSUserActivity *userActivity = nil;
-        if (code == INSendMessageIntentResponseCodeSuccess)
-            userActivity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass([INSendMessageIntent class])];
-        INSendMessageIntentResponse *response = [[INSendMessageIntentResponse alloc] initWithCode:INSendMessageIntentResponseCodeSuccess
-                                                                                     userActivity:userActivity];
-        completion(response);
-    };
-    
-    INPerson *person = intent.recipients.firstObject;
-    if (person && person.customIdentifier)
-    {
-        MXKAccount *account = [MXKAccountManager sharedManager].activeAccounts.firstObject;
-        MXFileStore *fileStore = [[MXFileStore alloc] initWithCredentials:account.mxCredentials];
-        [fileStore.roomSummaryStore fetchAllSummaries:^(NSArray<id<MXRoomSummaryProtocol>> * _Nonnull summaries) {
-            NSString *roomID = person.customIdentifier;
-            
-            BOOL isEncrypted = NO;
-            for (id<MXRoomSummaryProtocol> summary in summaries)
-            {
-                if ([summary.roomId isEqualToString:roomID])
-                {
-                    isEncrypted = summary.isEncrypted;
-                    break;
-                }
-            }
-
-            if (isEncrypted)
-            {
-                [MXFileStore setPreloadOptions:0];
-
-                MXSession *session = [[MXSession alloc] initWithMatrixRestClient:account.mxRestClient];
-                MXWeakify(session);
-                [session setStore:fileStore success:^{
-                    MXStrongifyAndReturnIfNil(session);
-
-                    self.selectedRoom = [MXRoom loadRoomFromStore:fileStore withRoomId:roomID matrixSession:session];
-
-                    // Do not warn for unknown devices. We have cross-signing now
-                    session.crypto.warnOnUnknowDevices = NO;
-
-                    MXWeakify(self);
-                    [self.selectedRoom sendTextMessage:intent.content
-                                              threadId:nil
-                                               success:^(NSString *eventId) {
-                        completeWithCode(INSendMessageIntentResponseCodeSuccess);
-                        MXStrongifyAndReturnIfNil(self);
-                        self.selectedRoom = nil;
-                    } failure:^(NSError *error) {
-                        completeWithCode(INSendMessageIntentResponseCodeFailure);
-                        MXStrongifyAndReturnIfNil(self);
-                        self.selectedRoom = nil;
-                    }];
-
-                } failure:^(NSError *error) {
-                    completeWithCode(INSendMessageIntentResponseCodeFailure);
-                }];
-
-                return;
-            }
-            
-            [account.mxRestClient sendTextMessageToRoom:roomID
-                                               threadId:nil
-                                                   text:intent.content
-                                                success:^(NSString *eventId) {
-                completeWithCode(INSendMessageIntentResponseCodeSuccess);
-            }
-                                                failure:^(NSError *error) {
-                completeWithCode(INSendMessageIntentResponseCodeFailure);
-            }];
-            
-        }];
-    }
-    else
-    {
-        completeWithCode(INSendMessageIntentResponseCodeFailure);
-    }
 }
 
 @end
