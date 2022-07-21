92fc7046ede2720d4b46bffd07d97ce59b50d95f
Doug <douglase@element.io>
2022-07-21 15:33:31 +0100

    Always perform the registration dummy stage immediately after the first one.

--- RiotSwiftUI/Modules/Authentication/Common/Service/MatrixSDK/RegistrationWizard.swift
+++ RiotSwiftUI/Modules/Authentication/Common/Service/MatrixSDK/RegistrationWizard.swift
@@ -268,17 +268,17 @@ class RegistrationWizard {
             let flowResult = authenticationSession.flowResult
             
             if isCreatingAccount || isRegistrationStarted {
-                return try await handleMandatoryDummyStage(flowResult: flowResult)
+                return try await handleDummyStage(flowResult: flowResult)
             }
             
             return .flowResponse(flowResult)
         }
     }
     
-    /// Checks for a mandatory dummy stage and handles it automatically when possible.
-    private func handleMandatoryDummyStage(flowResult: FlowResult) async throws -> RegistrationResult {
+    /// Checks for a dummy stage and handles it automatically when possible.
+    private func handleDummyStage(flowResult: FlowResult) async throws -> RegistrationResult {
         // If the dummy stage is mandatory, do the dummy stage now
-        guard flowResult.missingStages.contains(where: { $0.isDummy && $0.isMandatory }) else { return .flowResponse(flowResult) }
+        guard flowResult.missingStages.contains(where: { $0.isDummy }) else { return .flowResponse(flowResult) }
         return try await dummy()
     }
     
