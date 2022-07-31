bf0444c5749dd439d3ea879ea19acd7ddc26b511
vollkorntomate <thomas@vollkorntomate.de>
2022-07-31 18:06:39 +0200

    Show switch button if there are no public rooms on homeserver
    Fixes #4700
    
    Signed-off-by: Thomas Völkl thomas@vollkorntomate.de

--- Riot/Modules/Rooms/ShowDirectory/ShowDirectoryViewState.swift
+++ Riot/Modules/Rooms/ShowDirectory/ShowDirectoryViewState.swift
@@ -21,7 +21,6 @@ import Foundation
 /// ShowDirectoryViewController view state
 enum ShowDirectoryViewState {
     case loading
-    case loadedWithoutUpdate
     case loaded(_ sections: [ShowDirectorySection])
     case error(Error)
 }
