98338fd946e5d64e09f38c805a9a65935148c94f
ismailgulek <ismailg@matrix.org>
2022-08-12 17:46:44 +0300

    Remove unnecessary inputAccessoryView from room input toolbar

--- Riot/Modules/Room/Views/InputToolbar/RoomInputToolbarView.m
+++ Riot/Modules/Room/Views/InputToolbar/RoomInputToolbarView.m
@@ -80,10 +80,6 @@ static const NSTimeInterval kActionMenuComposerHeightAnimationDuration = .3;
     [self updateUIWithAttributedTextMessage:nil animated:NO];
     
     self.textView.toolbarDelegate = self;
-    
-    // Add an accessory view to the text view in order to retrieve keyboard view.
-    inputAccessoryView = [[UIView alloc] initWithFrame:CGRectZero];
-    self.textView.inputAccessoryView = inputAccessoryView;
 }
 
 - (void)setVoiceMessageToolbarView:(UIView *)voiceMessageToolbarView
