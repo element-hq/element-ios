35d4f8e5684ec4635f8d689893430ecf886b8f1f
SBiOSoftWhare <steveb@matrix.org>
2022-08-22 17:44:32 +0200

    Device manager: Create user sessions overview empty screen.

---
+++ RiotSwiftUI/Modules/UserSessions/UserSessionsOverview/View/UserSessionsOverview.swift
@@ -0,0 +1,47 @@
+// 
+// Copyright 2022 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import SwiftUI
+
+struct UserSessionsOverview: View {
+
+    // MARK: - Properties
+    
+    // MARK: Private
+    
+    @Environment(\.theme) private var theme: ThemeSwiftUI
+    
+    // MARK: Public
+    
+    @ObservedObject var viewModel: UserSessionsOverviewViewModel.Context
+    
+    var body: some View {
+        VStack {
+        }
+        .background(theme.colors.background)
+        .frame(maxHeight: .infinity)
+        .navigationTitle(VectorL10n.userSessionsOverviewTitle)
+    }
+}
+
+// MARK: - Previews
+
+struct UserSessionsOverview_Previews: PreviewProvider {
+    static let stateRenderer = MockUserSessionsOverviewScreenState.stateRenderer
+    static var previews: some View {
+        stateRenderer.screenGroup()
+    }
+}
