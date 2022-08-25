c5f50e62ce48e8553ff10ef2ef39c877ae75b9ef
Andy Uhnak <andyuhnak@gmail.com>
2022-08-25 10:15:03 +0100

    Upload Sentry Dsyms for App Store builds

--- Riot/Modules/Analytics/SentryMonitoringClient.swift
+++ Riot/Modules/Analytics/SentryMonitoringClient.swift
@@ -34,6 +34,7 @@ struct SentryMonitoringClient {
             options.dsn = Self.sentryDSN
             
             // Collecting only 10% of all events
+            options.sampleRate = 0.1
             options.tracesSampleRate = 0.1
             
             options.beforeSend = { event in
