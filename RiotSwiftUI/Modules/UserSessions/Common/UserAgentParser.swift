2683d4bdfd777a76ecf474548c91b36e1fb78658
ismailgulek <ismailg@matrix.org>
2022-10-03 14:29:00 +0300

    Update user agent parser to parse browser version too

--- RiotSwiftUI/Modules/UserSessions/Common/UserAgentParser.swift
+++ RiotSwiftUI/Modules/UserSessions/Common/UserAgentParser.swift
@@ -125,18 +125,28 @@ enum UserAgentParser {
     // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) ElementNightly/2022091301 Chrome/104.0.5112.102 Electron/20.1.1 Safari/537.36
     private static func parseDesktop(_ userAgent: String) -> UserAgent {
         var deviceOS: String?
-        let browserName = browserName(for: userAgent)
+        let browserInfo = browserInfo(for: userAgent)
 
         if let deviceInfo = findFirstDeviceInfo(in: userAgent) {
             let deviceInfoComponents = deviceInfo.components(separatedBy: "; ")
-            deviceOS = deviceInfoComponents[safe: 1]?.hasPrefix("Android") == true ? deviceInfoComponents[safe: 1] : deviceInfoComponents.first
+            if deviceInfoComponents[safe: 1]?.hasPrefix("Android") == true {
+                deviceOS = deviceInfoComponents[safe: 1]
+            } else if deviceInfoComponents.first == "Macintosh" {
+                var osFull = deviceInfoComponents[safe: 1]
+                osFull = osFull?.replacingOccurrences(of: "Intel ", with: "")
+                osFull = osFull?.replacingOccurrences(of: "Mac OS X", with: "macOS")
+                osFull = osFull?.replacingOccurrences(of: "_", with: ".")
+                deviceOS = osFull
+            } else {
+                deviceOS = deviceInfoComponents.first
+            }
         }
 
         return UserAgent(deviceType: .desktop,
-                         deviceModel: browserName,
+                         deviceModel: nil,
                          deviceOS: deviceOS,
-                         clientName: nil,
-                         clientVersion: nil)
+                         clientName: browserInfo.name,
+                         clientVersion: browserInfo.version)
     }
 
     // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36
@@ -164,20 +174,30 @@ enum UserAgentParser {
         return nil
     }
 
-    private static func browserName(for userAgent: String) -> String? {
+    private static func browserInfo(for userAgent: String) -> (name: String?, version: String?) {
         let components = userAgent.components(separatedBy: " ")
         if components.last?.hasPrefix("Firefox") == true {
-            return "Firefox"
+            let version = components.last?.components(separatedBy: "/").last
+            return ("Firefox", version)
         } else if components.last?.hasPrefix("Safari") == true
                     && components[safe:components.count - 2]?.hasPrefix("Mobile") == true {
             // mobile browser
-            let possibleBrowserName = components[safe:components.count - 3]?.components(separatedBy: "/").first
-            return possibleBrowserName == "Version" ? "Safari" : possibleBrowserName
+            let possibleBrowserComponents = components[safe:components.count - 3]
+            if possibleBrowserComponents?.hasPrefix("Version") == true {
+                let version = possibleBrowserComponents?.components(separatedBy: "/").last
+                return ("Safari", version)
+            } else {
+                let components = possibleBrowserComponents?.components(separatedBy: "/")
+                return (components?.first, components?.last)
+            }
         } else if components.last?.hasPrefix("Safari") == true && components[safe:components.count - 2]?.hasPrefix("Version") == true {
-            return "Safari"
+            let version = components[safe:components.count - 2]?.components(separatedBy: "/").last
+            return ("Safari", version)
         } else {
             // regular browser
-            return components[safe:components.count - 2]?.components(separatedBy: "/").first
+            let browserComponent = components[safe:components.count - 2]
+            let components = browserComponent?.components(separatedBy: "/")
+            return (components?.first, components?[safe: 1])
         }
     }
 }
