d1ce571ab1b48b09a67640d359a15327f7977c38
Giom Foret <giom@matrix.org>
2022-10-19 16:07:26 +0200

    Update the Voice Broadcast Labs flag description

--- Riot/Generated/Strings.swift
+++ Riot/Generated/Strings.swift
@@ -7535,7 +7535,7 @@ public class VectorL10n: NSObject {
   public static var settingsLabsEnableThreads: String { 
     return VectorL10n.tr("Vector", "settings_labs_enable_threads") 
   }
-  /// Voice broadcast (under active development). We currently only detect voice broadcast in the room timeline, this is not possible to send or listen an actual voice broadcast
+  /// Voice broadcast (under active development)
   public static var settingsLabsEnableVoiceBroadcast: String { 
     return VectorL10n.tr("Vector", "settings_labs_enable_voice_broadcast") 
   }
