3552ed75220a1f623673853cb0636a6192d398e5
Mauro Romito <mauro.romito@element.io>
2022-12-15 01:44:26 +0100

    removed unused code

--- Riot/Modules/MatrixKit/Utils/EventFormatter/HTMLFormatter.swift
+++ Riot/Modules/MatrixKit/Utils/EventFormatter/HTMLFormatter.swift
@@ -114,27 +114,3 @@ extension HTMLFormatter {
         return stringBuilder?.generatedAttributedString()
     }
 }
-
-extension UIColor {
-    func toHexString() -> String {
-        var r:CGFloat = 0
-        var g:CGFloat = 0
-        var b:CGFloat = 0
-        var a:CGFloat = 0
-
-        getRed(&r, green: &g, blue: &b, alpha: &a)
-
-        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
-
-        return NSString(format:"#%06x", rgb) as String
-    }
-
-    convenience init(red: Int, green: Int, blue: Int) {
-        assert(red >= 0 && red <= 255, "Invalid red component")
-        assert(green >= 0 && green <= 255, "Invalid green component")
-        assert(blue >= 0 && blue <= 255, "Invalid blue component")
-
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-    }
-
-}
