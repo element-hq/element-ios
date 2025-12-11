ede2db6c0c249aa15ffb632d933a891b5ebfb6ba
Mauro Romito <mauro.romito@element.io>
2025-12-11 10:49:10 +0100

    update copyright

--- Riot/Modules/LocationSharing/LocationManager.swift
+++ Riot/Modules/LocationSharing/LocationManager.swift
@@ -1,5 +1,6 @@
 //
-// Copyright 2022-2024 New Vector Ltd.
+// Copyright 2025 Element Creations Ltd.
+// Copyright 2022-2025 New Vector Ltd.
 //
 // SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 // Please see LICENSE files in the repository root for full details.
